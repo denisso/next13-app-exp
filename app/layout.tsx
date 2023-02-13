07f8d8e1f1ab2cc933d9efd041ab6c4bc3cf52cf
Denis <like-nix@mail.ru>
2023-02-13 16:21:07 +0300

    removed test data

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossOrigin="anonymous"
-        />
-      </head>
+      <head></head>
       <body>{children}</body>
     </html>
   );
