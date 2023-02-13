091f47e0bd96f67383ab3a4a0da26fddb01d888d
Denis <like-nix@mail.ru>
2023-02-13 16:20:29 +0300

    not finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossOrigin="anonymous"
-        />
       </head>
       <body>{children}</body>
     </html>
