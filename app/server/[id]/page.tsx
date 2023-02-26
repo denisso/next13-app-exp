68a664cfaef44c08bfbb8bcdfa3e4be8e5fa3af9
Denis <like-nix@mail.ru>
2023-02-26 22:31:04 +0300

    update
    add schema for Vercel url

--- app/server/[id]/page.tsx
+++ app/server/[id]/page.tsx
@@ -4,7 +4,7 @@ import { EFetchSide } from "@/components/types";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const data = await fetchData(
-    process?.env?.NEXT_PUBLIC_VERCEL_URL + "/api/" + params.id
+    `${process?.env?.SCHEMA_URL}${process?.env?.VERCEL_URL}/api/${params.id}`
   );
   return (
     <ClientComponent id={params.id} data={data} side={EFetchSide.server} />
